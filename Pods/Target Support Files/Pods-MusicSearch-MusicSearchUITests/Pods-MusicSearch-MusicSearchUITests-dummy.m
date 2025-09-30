01f456f3eee0479631f3a9432a11b692ae7e7c20
Lalannns <allan2006j@gmail.com>
2025-09-30 12:50:30 +0500

    Delete Pods directory

--- Pods/Target Support Files/Pods-MusicSearch-MusicSearchUITests/Pods-MusicSearch-MusicSearchUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicSearch_MusicSearchUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MusicSearch_MusicSearchUITests
-@end
--- Pods/Target Support Files/Pods-MusicSearchTests/Pods-MusicSearchTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicSearchTests : NSObject
-@end
-@implementation PodsDummy_Pods_MusicSearchTests
-@end
